d29f31aa0ff1f85ae2a34a0ced3dc19e4b254540
alice <57547425+AliceDTRH@users.noreply.github.com>
2026-05-25 01:21:01 -0400

    Convert project to CMAKE fully
    I removed the old binary locations from .gitignore and
    updated build.sh to use cmake

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@
 
 #gcc src/tritron.c src/trit.c src/trit.h -o tritron -Wall -Wextra --verbose --pedantic
 #gcc src/tritron.c src/trit.c src/trit.h -o tritron -Wall -Wextra 
-gcc src/tritlib/*.c src/tritlib/*.h src/*.c -o tritron -Wall -Wextra -lm
+#gcc src/tritlib/*.c src/tritlib/*.h src/*.c -o tritron -Wall -Wextra -lm
+
+cmake -B build
+cmake --build build
